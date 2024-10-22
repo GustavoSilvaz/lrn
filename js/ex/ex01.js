bceedc16a3046e2e86557a6d6e9b3801885c36de
Gustavo Silva <guh_sz@outlook.com>
2024-10-22 17:33:08 -0300

    att

---
+++ js/ex/ex01.js
@@ -0,0 +1,4 @@
+i=100
+while(i<=200);
+i+=20
+console.log(i)
---
+++ py/mch/01.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+base_credit = pd.read_csv('C:\Users\Guh_S\Documents\GitHub\lrn\py\material\Machine Learning e Data Science com Python de A à Z\Bases de dados\credit_data.csv')
+
+base_credit #defaulted
